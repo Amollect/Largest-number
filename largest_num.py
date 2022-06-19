6c68f896645ddedeabed398e9ade75f213771fbe
Amol Shinde <107493995+Amollect@users.noreply.github.com>
2022-06-19 12:41:57 +0530

    Add files via upload

---
+++ largest_num.py
@@ -0,0 +1,7 @@
+#find largest number without using 'and' and 'or' operation
+
+a=int(input("enter 1st number:"))
+b=int(input("enter 2nd number:"))
+c=int(input("enter 3rd number:"))
+list=[a,b,c]
+print("largest element is:", max(list))
